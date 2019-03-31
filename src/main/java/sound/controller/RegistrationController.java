cd4bd30dcd576ec84b105bc8fc824f0a99f36721
Sergey <8599itotus@gmail.com>
2019-03-31 18:09:04 +0300

    init

---
+++ src/main/java/sound/controller/RegistrationController.java
@@ -0,0 +1,36 @@
+package sound.controller;
+
+import sound.domain.Role;
+import sound.domain.User;
+import sound.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.Collections;
+
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepo UserRepo;
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+    @PostMapping("/registration")
+
+    public String addUser(User user, Map<String,Object>model){
+        User UserFromDb = UserRepo.findByEmail(user.getEmail());
+        if (UserFromDb!=null){
+            model.put("message","User exist!");
+            return "/registration";
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        UserRepo.save(user);
+        return "redirect:/login";
+    }
+
+}
