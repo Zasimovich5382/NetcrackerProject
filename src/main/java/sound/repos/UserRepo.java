8b0eced1b42babaac4c501aa60d2927c80798239
Sergey <8599itotus@gmail.com>
2019-04-09 20:53:47 +0300

    fixAuthorization

--- src/main/java/sound/repos/UserRepo.java
+++ src/main/java/sound/repos/UserRepo.java
@@ -3,5 +3,5 @@ import sound.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    User findByUsername(String user);
 }
